e9b9a1c1d2c575eeccb50272d98a519d84b87135
soumya <saurabh@nettrackers.net>
2021-07-02 00:24:34 +0530

    test to commit

---
+++ project-1.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+
+    <?php include("include/css.php");?>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.css"/>
+
+    <title>Ramindri Group</title>
+  </head>
+  <body>
+
+
+ 
+    <?php include("include/header.php");?> 
+
+ <section class="topBannerSec">
+     <div class="imgBox"><img src="images/project1-banner.jpg"></div>
+     <div class="inner-title">
+        <h1>Project One</h1>
+        <!-- <p><span>10000+</span> Happy Families</p> -->
+      </div>
+ </section>   
+
+ <section class="project-inner-1">
+   <div class="container aos-animation">
+      <div class="row">
+         <div class="col-sm-12">
+           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#overview">LOGO</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+   <!--  <span class="navbar-toggler-icon"></span> -->
+   <i class="fas fa-bars"></i>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#gallery">Gallery <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#location">Location</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#floorPlan">Floor Plans</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#specifications">Specifications</a>
+      </li>
+
+
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+      <div class="clearfix"></div> 
+    </ul>
+  </div>
+  <div class="clearfix"></div> 
+</nav>
+<div class="clearfix"></div> 
+         </div>
+      </div>
+   </div>  
+   <div class="clearfix"></div>       
+ </section>
+
+<section>
+  <section id="overview" class="pad_bt">
+    <div class="container aos-animation">
+      <h2 class="inner-text_style_1">Overview</h2>
+      <div class="row">
+          <div class="col-sm-2"></div>
+          <div class="col-sm-4">
+              <div class="overview_style_1">
+                  <h2>95</h2>
+                  <p>Amenities &amp; facilities</p>
+              </div>
+
+              <div class="overview_style_1">
+                  <h2>255</h2>
+                  <p>Apartments</p>
+              </div>
+
+              <div class="overview_style_1">
+                  <h2>2/3</h2>
+                  <p>BHK homes</p>
+              </div>
+
+              <div class="overview_style_1">
+                  <h2>4</h2>
+                  <p>Towers</p>
+              </div>
+          </div>
+          <div class="col-sm-6">
+              <p class="mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
+          </div>
+      </div>
+    </div>
+  </section>
+</section>
+
+
+<section id="gallery">
+  <div class="container aos-animation">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2 class="inner-text_style_1">Gallery</h2>
+      </div>
+      <div class="clearfix"></div>
+      <div class="col-sm-4">
+        <a href="images/blog-3.jpg" data-fancybox="gal1" data-caption="Picture 1">
+          <img src="images/blog-3.jpg" class="img-fluid">
+        </a> 
+      </div>
+      <div class="col-sm-4">
+        <a href="images/blog-4.jpg" data-fancybox="gal1" data-caption="Picture 2">
+          <img src="images/blog-4.jpg" class="img-fluid">
+        </a>
+      </div>
+      <div class="col-sm-4">
+        <a href="images/blog-5.jpg" data-fancybox="gal1" data-caption="Picture 3">
+          <img src="images/blog-5.jpg" class="img-fluid">
+         </a>
+      </div>
+      <div class="col-sm-4">
+        <a href="images/blog-6.jpg" data-fancybox="gal1" data-caption="Picture 4">
+        <img src="images/blog-6.jpg" class="img-fluid">
+       </a>
+      </div>
+      <div class="col-sm-4">
+        <a href="images/blog-7.jpg" data-fancybox="gal1" data-caption="Picture 5">
+        <img src="images/blog-7.jpg" class="img-fluid">
+       </a>
+      </div>
+      <div class="col-sm-4">
+        <a href="images/blog-8.jpg" data-fancybox="gal1" data-caption="Picture 6">
+        <img src="images/blog-8.jpg" class="img-fluid">
+      </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<section id="about" class="pad_bt">
+    <div class="container aos-animation">
+      <h2 class="inner-text_style_1">About Us</h2>
+      <div class="row">
+        <div class="col-sm-3"><img src="images/logo.png" class="img-fluid"></div>
+        <div class="col-sm-9">
+          <p> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+          <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-sm-12">
+          <p> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. </p>
+          <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?</p>
+          <a href="#" target="_blank">www.ramindrigroup.co.in</a> </div>
+      </div>
+    </div>
+  </section>
+
+
+<section id="location">
+    <h2 class="inner-text_style_1">Location</h2>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17572.68798893017!2d91.74149327717517!3d26.140672353718355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a5a287f9133ff%3A0x2bbd1332436bde32!2sGuwahati%2C%20Assam!5e0!3m2!1sen!2sin!4v1620286655168!5m2!1sen!2sin" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+</section>
+
+
+  
+<section id="floorPlan" class="pad_bt pb-5">
+    <div class="container aos-animation">
+        <h2 class="inner-text_style_1">Floor Plan</h2>
+        <div class="row">
+            <div class="col-6 col-md-6 col-lg-3">
+                <a href="images/1.jpg" data-fancybox="gal2" data-caption="Picture 1">
+                <img src="images/1.jpg" class="img-fluid">
+              </a>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/2.jpg" data-fancybox="gal2" data-caption="Picture 2">
+                <img src="images/2.jpg" class="img-fluid">
+              </a>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/3.jpg" data-fancybox="gal2" data-caption="Picture 3">
+                <img src="images/3.jpg" class="img-fluid">
+              </a>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/4.jpg" data-fancybox="gal2" data-caption="Picture 4">
+                <img src="images/4.jpg" class="img-fluid">
+              </a>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <br><br>     
+        <div class="row">
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/5.jpg" data-fancybox="gal2" data-caption="Picture 5">
+                <img src="images/5.jpg" class="img-fluid">
+              </a>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/6.jpg" data-fancybox="gal2" data-caption="Picture 6">
+                <img src="images/6.jpg" class="img-fluid">
+              </a>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/7.jpg" data-fancybox="gal2" data-caption="Picture 7">
+                <img src="images/7.jpg" class="img-fluid">
+              </a>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <a href="images/8.jpg" data-fancybox="gal2" data-caption="Picture 8">
+                <img src="images/8.jpg" class="img-fluid">
+              </a>
+            </div>
+        </div>
+    </div>    
+</section>
+
+
+
+<section id="specifications" class="pad_bt">
+    <div class="content">
+        <div class="container aos-animation">
+            <h2 class="inner-text_style_1">Specifications</h2>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2 class="text_style_3">Sub Structure and Super Structure</h2>
+                    <p>
+                        Suitable Foundation as per Structural Design (conforming with IS code) with Pesticide Treatment.
+                    </p>
+
+                    <p>
+                        RCC Frame Super - Structure as per Seismic Zone 3 compliance with Bricks / Blocks.
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="text_style_3">Painting</h2>
+                    <p>
+                        Interior Walls : Double Putty Finished walls with Two Coats plastic emulsion.
+                    </p>
+                    <p>
+                        Ceiling : Cement Paint over Primer.
+                    </p>
+                    <p>
+                        Exterior Walls : Plastered and Cement Paint.
+                    </p>
+                    <p>
+                        Stilt Floor Ceiling and Common Area Walls - Plastered and Cement Paint.
+                    </p>
+                </div>    
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2 class="text_style_3">Flooring &amp; Skirting</h2>
+                    <p>
+                        2' x 2' (600 x 600) Vitrified Tile for Living, Dining, Kitchen, and Bedroom.
+                    </p>
+                    <p>
+                        1' x 1' (300 x 300) Antiskid Tile for Bathroom, Utility, and Balcony.
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="text_style_3">Water - Proofing</h2>
+                    <p>
+                        Water Proofing in Bathroom &amp; Terrace.
+                    </p>
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2 class="text_style_3">Kitchen &amp; Utility</h2>
+                    <p>
+                        Glazed wall tiles, up to 2 feet height, above kitchen counter.
+                    </p>
+
+                    <p>
+                        Granite Counter Top over RCC Slab with Stainless Steel Single Bowl Sink.
+                    </p>
+
+                    <p>
+                        Provision for Water Purifier, Aqua Guard &amp; Exhaust Fan. 
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <h2 class="text_style_3">Electrical</h2>
+                    <p>
+                        All internal and external PVC Conduits with ISI brand wire.
+                    </p>
+
+                    <p>
+                        Modular switches of Anchor Roma / Panasonic / MK or Equivalent.
+                    </p>
+
+                    <p>3 Phase connection for each apartment.</p>
+                    <p>Internal Conduit for Air-conditioner.</p>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <br><br>
+        </div>
+    </div>
+</section>
+
+
+<section id="contact" class="pad_bt">
+    <div class="content">
+        <div class="container aos-animation">
+            <h2 class="inner-text_style_1">Contact</h2>
+            <div class="row">
+                <div class="col-sm-5">
+                    <h2 class="text_style_3">Site Address</h2>
+                    <p> Unit No. 702B, 6th floor, Central Mall,<br>
+                        Christian Basti, G S Road, Guwahati,<br>
+                        Assam. PIN - 781005
+                    </p>
+
+                    <h2 class="text_style_3">Sales Team</h2>
+                    <p> 0361-7150633<br>
+                        info@ramindrigroup.com
+                    </p>
+                </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-5">
+                    <h2 class="text_style_3">Corp. Office</h2>
+                    <p>Unit No. 702B, 6th floor, Central Mall,<br>
+                        Christian Basti, G S Road, Guwahati,<br>
+                        Assam. PIN - 781005
+                    </p>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <br><br>
+        </div>
+    </div>
+</section>
+
+
+
+<?php include("include/footer.php");?>
+
+<?php include("include/js.php");?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.js"></script>
+    
+<script type="text/javascript">
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50){ 
+        $('body').addClass("header-fixed");
+    }
+    else{
+        $('body').removeClass("header-fixed");
+    }
+
+    if ($(this).scrollTop() > 290){ 
+        $('.project-inner-1').addClass("sticky");
+    }
+    else{
+        $('.project-inner-1').removeClass("sticky");
+    }
+});
+
+
+
+$('.page-scroll').click(function(){
+    $("html, body").animate({ scrollTop: $(window).height()}, 1000);
+    return false;
+});
+</script>
+    
+  </body>
+</html>
